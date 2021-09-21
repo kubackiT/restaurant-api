ba3e1cdd3c626a0a22b0f45534f34839301c3df3
kubackiT <80779935+kubackiT@users.noreply.github.com>
2021-09-21 20:36:37 +0200

    adding spring boot project

---
+++ src/main/java/com/tomaszkubacki/RstraurantAPI/RstraurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.tomaszkubacki.RstraurantAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RstraurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RstraurantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomaszkubacki/RstraurantAPI/RstraurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaszkubacki.RstraurantAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RstraurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
